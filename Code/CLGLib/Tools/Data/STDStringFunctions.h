52d616280db405e9c47ddc3d8164f7d753731ed2
NBAlexis <nbalexis@gmail.com>
2021-08-15 05:07:37 +0800

    small bug fix

--- Code/CLGLib/Tools/Data/STDStringFunctions.h
+++ Code/CLGLib/Tools/Data/STDStringFunctions.h
@@ -53,9 +53,9 @@ inline INT vsnprintf_s(TCHAR* buffer, size_t sizeOfBuffer, const TCHAR* format, 
 
 inline TCHAR* strcpy_s(TCHAR* dest, size_t length, const TCHAR* source)
 {
-    size_t lengthOfdest = static_cast<size_t>(appStrlen(dest))
+    size_t lengthOfdest = static_cast<size_t>(appStrlen(dest));
     if (lengthOfdest < length)
-        length = lengthOfdest
+        length = lengthOfdest;
     return std::strncpy(dest, source, length);
 }
 
